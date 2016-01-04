5b05f223674df6e94669a72fe5d34004c61f84be
AdeniaGit <frederic.grochowski@adenia.fr>
2016-01-04 18:42:36 +0100

    update dec 2015

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,12 +14,14 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-		/*
+
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));*/
+        ));
+        /*
 		 return $this->render('default/temporary.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
+		*/
     }
 }
