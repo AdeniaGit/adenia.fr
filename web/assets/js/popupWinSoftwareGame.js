da1886988e959866715db031ec11178ad0eea64d
AdeniaGit <frederic.grochowski@adenia.fr>
2016-01-18 20:04:11 +0100

    Ajout d'erreurs pour la popup

--- web/assets/js/popupWinSoftwareGame.js
+++ web/assets/js/popupWinSoftwareGame.js
@@ -37,7 +37,7 @@ function closeWinSoftwareGame()
 
 var inpopup = false;
 // fonction déclenchée à l'entrée de la souris dans le body de la popup. Elle permet de déplacer aléatoirement la popup
-// et d'en changer le texte au bout de 0,5s.
+// et d'en changer le texte au bout de 0,4s.
 function attemptCloseWinSoftwareGame()
 {
     if(inpopup == false)
@@ -62,6 +62,6 @@ function attemptCloseWinSoftwareGame()
             popup.css({top: newTop+'px', left: newLeft+'px', 'margin': '0'});
 
             inpopup = false;
-        }, 300);
+        }, 400);
     }
 }
