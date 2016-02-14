022fbefb2c8e100c78fd150028b156e9066becf9
AdeniaGit <frederic.grochowski@adenia.fr>
2016-02-14 17:00:41 +0100

    Page d'accueil du site pour mise en prod

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,13 +13,13 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        /*
         return $this->render('default/temporary.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
-        /*
+        */
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
-        */
     }
 }
